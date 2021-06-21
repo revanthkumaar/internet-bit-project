39eace5b5f12bd94c22d0669f176534595242bef
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-21 19:51:09 +0530

    shop page created
    shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/home-page/home-page.component'
-
+import ShopPage from './pages/shop-page/shop-page.component'
 function App() {
   return (
     <div className="App">
       <HomePage/>
+      <ShopPage/>
     </div>
   );
 }
--- src/pages/shop-page/shop-page.component.js
+++ src/pages/shop-page/shop-page.component.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import './shop-page.styles.css'
+
+const ShopPage = () => {
+    return (
+
+        <div className="homepage">
+        <p>this is shop page</p>
+    </div>
+
+    )
+   
+}
+
+export default ShopPage;
