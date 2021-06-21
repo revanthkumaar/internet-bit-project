038fc47621c3e01b33a32fe89c30914a8f99b837
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-21 19:46:51 +0530

    home page compoent checking
    lkajhsdl

---
+++ src/pages/home-page/home-page.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home-page.styles.css'
+
+const HomePage = () => {
+    return (
+
+        <div className="homepage">
+        <p>this is home page</p>
+    </div>
+
+    )
+   
+}
+
+export default HomePage;
