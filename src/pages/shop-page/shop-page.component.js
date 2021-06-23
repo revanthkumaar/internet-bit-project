8d68184896ecb32bc2938555e0458098cb16f225
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-23 20:42:04 +0530

    with collection preview and collection item

--- src/pages/shop-page/shop-page.component.js
+++ src/pages/shop-page/shop-page.component.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import './shop-page.styles.css'
+import './shop-page.styles.scss'
+import SHOP_DATA from './shop.data.js';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-const ShopPage = () => {
-    return (
 
-        <div className="shop-page">
-        <p>this is shop page</p>
-    </div>
+class ShopPage extends React.Component{
 
-    )
-   
+    constructor(props){
+        super(props);
+
+        this.state = {
+            collections : SHOP_DATA,
+            number:123
+        }
+    }
+
+    render(){
+
+        //object de-structuring
+        const {collections, number} = this.state;
+       // const {number} = this.state;
+
+        return(
+            <div className="shop-page">
+
+               {collections.map(({ id, ...otherCollectionProps }) => (
+                         <CollectionPreview key={id} {...otherCollectionProps} />
+                ))}
+
+            </div>
+        )
+    }
 }
 
 export default ShopPage;
