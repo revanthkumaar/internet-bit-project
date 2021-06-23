0b44e25a79a5cb461b59c9ceac098c9cbf988368
revanthkumaar <iamrevanthkumar@gmail.com>
2021-06-23 19:38:09 +0530

    added dynamic routing

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import './menu-item.styles.scss';
-
-const MenuItem = ({title, imageUrl}) => {
+import {withRouter} from 'react-router-dom'
+ 
+const MenuItem = ({title, imageUrl, linkUrl, history, match, location}) => {
+    console.log(history)
+        console.log(match)
+            console.log(location)
 
     return(
 
-        <div className="menu-item">
+        <div className="menu-item"
+        onClick={ () => history.push(`${match.url}${linkUrl}`) }>
 
-        <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='title'>
-        <h1>{title.toUpperCase()}</h1>
+                <div
+              className='background-image'
+              style={{
+                backgroundImage: `url(${imageUrl})`
+              }}
+            />
+            <div className='title'>
+                <h1>{title.toUpperCase()}</h1>
+            </div>
+
     </div>
-
-    </div>
-
-
     )
-
-   
-
-
 }
-
-export default MenuItem;
+export default withRouter(MenuItem); //wrap your component inside the higher order component
