77ca3a4448c12ac3f2752bd5a6a558bcacda1e53
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-06 19:29:49 +0530

    hooks useeffec

--- src/components/books/createBookhooksversion.js
+++ src/components/books/createBookhooksversion.js
@@ -1,26 +1,36 @@
-import React, {useEffect} from 'react';
-import {useSelector, useDispatch} from 'react-redux'
-import allActions from './actions/index.js'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import allActions from "./actions/index.js";
 
 const createBook = () => {
-  const counter = useSelector(state => state.counter)
-     const currentUser =
+  const counter = useSelector((state) => state.counter);
+  const currentUser = useSelector((state) => state.currentUser);
+  const dispatch = useDispatch(); //initializing dispatcher from this comp
+  const user = { name: "revanth" };
 
-     const dispatch = useDispatch();
-
-     const user = {name: 'revanth'}
-
-     return (
-
-          <div className="createBook-hooks">
-                    <h1> Counter: {counter}</h1>
-               <button onClick = {allActions.counterActions.incrementCount()}></button>
-          </div>
-
-     )
+  //creating a side effect
+  useEffect(() => {
+       
+  }, []);
 
 
+  return (
+    <div className="createBook-hooks">
+
+         {
+              currentUser.loggedIn ? 
+
+              <>
+                    <h1> Hello, {currentUser.name}  </h1>
+
+                    <button onClick={() => dispatch(allActions.userActions.logOut())}>Log out</button>
+              </>
+         }
 
 
 
-}
+      <h1> Counter: {counter}</h1>
+      <button onClick={allActions.counterActions.incrementCount()}></button>
+    </div>
+  );
+};
