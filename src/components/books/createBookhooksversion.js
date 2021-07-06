22f391c9ca5ac65d094bee92fedd614358ffdf49
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-06 19:35:37 +0530

    asd

--- src/components/books/createBookhooksversion.js
+++ src/components/books/createBookhooksversion.js
@@ -1,36 +1,59 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import allActions from "./actions/index.js";
+import "./App.css";
+import allActions from "./actions";
 
-const createBook = () => {
+const App = () => {
   const counter = useSelector((state) => state.counter);
   const currentUser = useSelector((state) => state.currentUser);
-  const dispatch = useDispatch(); //initializing dispatcher from this comp
-  const user = { name: "revanth" };
 
-  //creating a side effect
+  const dispatch = useDispatch();
+
+  const user = {
+    name: "Revanth",
+  };
+
   useEffect(() => {
-       
+    dispatch(allActions.userActions.setUser(user));
   }, []);
 
-
   return (
-    <div className="createBook-hooks">
-
-         {
-              currentUser.loggedIn ? 
-
-              <>
-                    <h1> Hello, {currentUser.name}  </h1>
-
-                    <button onClick={() => dispatch(allActions.userActions.logOut())}>Log out</button>
-              </>
-         }
-
-
-
-      <h1> Counter: {counter}</h1>
-      <button onClick={allActions.counterActions.incrementCount()}></button>
+    <div className="App">
+      {" "}
+      {currentUser.loggedIn ? (
+        <>
+          <h1>
+            {" "}
+            Hello,
+            {currentUser.user.name}{" "}
+          </h1>{" "}
+          <button onClick={() => dispatch(allActions.userActions.logOut())}>
+            {" "}
+            Logout{" "}
+          </button>{" "}
+        </>
+      ) : (
+        <>
+          <h1> Login </h1>{" "}
+          <button
+            onClick={() => dispatch(allActions.userActions.setUser(user))}
+          >
+            {" "}
+            Login as Revanth{" "}
+          </button>{" "}
+        </>
+      )}{" "}
+      <h1> Counter: {counter} </h1>{" "}
+      <button onClick={() => dispatch(allActions.counterActions.increment())}>
+        {" "}
+        Increase Counter{" "}
+      </button>{" "}
+      <button onClick={() => dispatch(allActions.counterActions.decrement())}>
+        {" "}
+        Decrease Counter{" "}
+      </button>{" "}
     </div>
   );
 };
+
+export default App;
